726e40e7112c46434b89673483eea5971ce82dcc
Dmytro <dima.shutko5@gmail.com>
2026-03-31 20:12:43 +0200

    solution3

--- task.sql
+++ task.sql
@@ -17,14 +17,21 @@ CREATE TABLE Warehouses (
     FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION
 );
 
+CREATE TABLE Product (
+    ID INT AUTO_INCREMENT,
+    PRIMARY KEY (ID),
+    Name VARCHAR(50)
+);
 CREATE TABLE ProductInventory (
     ID INT AUTO_INCREMENT,
     PRIMARY KEY (ID),
-    Name VARCHAR(50),
     Amount INT,
     WarehouseID INT,
-    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
+    ProductId INT,
+    FOREIGN KEY (ProductId) REFERENCES Product(ID) ON DELETE CASCADE
 );
+
 
 INSERT INTO Countries (NAME)
     VALUES ('Country1'), ('Country2');
@@ -33,5 +40,10 @@ INSERT INTO Countries (NAME)
 INSERT INTO Warehouses (WarehouseName, WarehouseAddress, CountryID)
     VALUES ('Warehouse-1', 'City-1, Street-1', 1), ('Warehouse-2', 'City-2, Street-1', 2);
 
-INSERT INTO ProductInventory (Name, Amount, WarehouseID)
-    VALUES ('Product-1', 2, 1), ('Product-2', 5, 2);
+INSERT INTO Product (Name)
+    VALUES ('Product-1'), ('Product-2');
+
+INSERT INTO ProductInventory (WarehouseID, ProductID, Amount)
+    VALUES (1, 1, 2), (1, 1, 2);
+
+
